198153dc6500c84c1c40d78ba5b752e63c6b0c5c
олегсий <alex181211@gmail.com>
2024-04-30 23:35:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import mysql.connector as mc
+
+connection = mc.connect(
+    host="localhost",
+    user="",
+    password=""
+)
+
+
+'''
+cursor = db.cursor()
+
+cursor.execute("SHOW DATABASES")
+for i in cursor:
+    print(i)
+
+'''
