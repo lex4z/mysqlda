ed3ab940aea93b43677c000b5d7e9ad2cc77fdc2
олегсий <alex181211@gmail.com>
2024-05-01 00:13:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import mysql.connector as mc
+import json
 
-connection = mc.connect(
-    host="localhost",
-    user="",
-    password=""
+
+_host = "localhost"
+_user = "root"
+_password = ""
+_database = "anna"
+
+db = mc.connect(
+    host = _host,
+    user = _user,
+    password = _password,
+    database = _database
 )
+
+cursor = db.cursor()
+cursor.execute("SELECT Educational_Track_Name FROM oop WHERE Admission_year>0")
+oop_names = [i for i in cursor]
+#oop_names = oop_names[2:]
+
+print(oop_names)
 
 
 '''
