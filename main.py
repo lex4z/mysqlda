8e08d06c8d1c2b4767293b67fa4247ad80e09f88
олегсий <alex181211@gmail.com>
2024-05-01 01:12:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,25 @@ with open("config.json") as f:
 
 db = mc.connect(**config)
 
+root = tk.Tk()
+root.title("window")
+
+
 cursor = db.cursor()
 cursor.execute("SELECT Educational_Track_Name FROM oop")
 oop_names = [i[0] for i in cursor]
 
-print(oop_names)
+def show(text):
+    lbl1.config(text = text)
+
+clicked = tk.StringVar()
+drop = tk.OptionMenu(root, clicked, *oop_names, command=show)
+drop.pack()
+
+#btn = tk.Button(root, text = "reload", command=show).pack()
+
+lbl1 = tk.Label(root, text = " ")
+lbl1.pack()
+
+root.mainloop()
+#print(oop_names)
