6ca6bc0c2fca7635caa0206aac2cc9c1c3a536fe
олегсий <alex181211@gmail.com>
2024-05-01 14:21:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mysql.connector as mc
 import tkinter as tk
+from tkinter import ttk
 import json
 
 with open("config.json") as f:
@@ -9,18 +10,21 @@ db = mc.connect(**config)
 
 root = tk.Tk()
 root.title("window")
+root.geometry("400x200")
 
 
 cursor = db.cursor()
 cursor.execute("SELECT Educational_Track_Name FROM oop")
 oop_names = [i[0] for i in cursor]
 
-def show(text):
-    lbl1.config(text = text)
+def show(event):
+    lbl1.config(text = drop.get())
 
 clicked = tk.StringVar()
-drop = tk.OptionMenu(root, clicked, *oop_names, command=show)
+#drop = tk.OptionMenu(root, clicked, *oop_names, command=show)
+drop = ttk.Combobox(root, values=oop_names, textvariable=clicked,state="readonly")
 drop.pack()
+drop.bind("<<ComboboxSelected>>", show)
 
 #btn = tk.Button(root, text = "reload", command=show).pack()
 
