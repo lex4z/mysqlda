397e5753c375083679a9db89399e00a5813fb1d0
олегсий <alex181211@gmail.com>
2024-05-01 15:04:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,29 @@ root = tk.Tk()
 root.title("window")
 root.geometry("400x200")
 
-
 cursor = db.cursor()
-cursor.execute("SELECT Educational_Track_Name FROM oop")
-oop_names = [i[0] for i in cursor]
+cursor.execute("SELECT Educational_Track_Name, Educational_Programm_Name FROM oop ORDER BY Educational_Programm_Name")
+
+et_names = []
+oop_names = []
+oop_ids = []
+
+for (i,j) in cursor:
+    et_names.append(i)
+    oop_names.append(j)
+
+cursor.execute("SELECT ID_OOP FROM real_oop ORDER BY Educational_Programm_Name")
+oop_ids = [i[0] for i in cursor]
+
+print(et_names,oop_names,oop_ids)
 
 def show(event):
     lbl1.config(text = drop.get())
 
 clicked = tk.StringVar()
 #drop = tk.OptionMenu(root, clicked, *oop_names, command=show)
-drop = ttk.Combobox(root, values=oop_names, textvariable=clicked,state="readonly")
+
+drop = ttk.Combobox(root, values = et_names, textvariable = clicked,state = "readonly")
 drop.pack()
 drop.bind("<<ComboboxSelected>>", show)
 
