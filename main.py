f63fa9770eab66c411e507ba7cd2f5baeb7eaf02
олегсий <78318213+lex4z@users.noreply.github.com>
2024-12-10 00:38:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ root.title("window")
 #screen_height = root.winfo_screenheight()
 
 window_width = 700
-window_height = 300
+window_height = 400
 
 root.geometry(f"{window_width}x{window_height}")
 
@@ -44,83 +44,84 @@ for i in range(len(temp[0])):
     cursor.execute(f"UPDATE rpd SET stavka_part = {temp[1][i]} WHERE Discipline_ID = {temp[0][i]}")
     db.commit()
 
-def drop1_selected(event):
-    btn1.config(state="normal")
-    btn1.focus_force()
+def oop_selected(event):
+    get_project_indicators_btn.config(state="normal")
+    get_project_indicators_btn.focus_force()
 
 
-def btn1_click():
-    lbl3.pack_forget()
-    btn2.pack_forget()
-    global all_sum, partial_sum1, partial_sum2, n1, n2
-    selected_oop_id = oop_ids[drop1_text.get()]
-    cursor.execute(f"SELECT Discipline_laboratory,Discipline_lectures,Discipline_practices stavka_part FROM rpd WHERE ID_OOP = {selected_oop_id}")
-    all_sum = sum([sum(i) for i in cursor])
+def get_project_indicators():
+    recomendations_label.pack_forget()
+    get_recomendations_btn.pack_forget()
+    global all_hours, degree_hours, pract_hours, n1, n2
+    selected_oop_id = oop_ids[oop_drop_text.get()]
+    cursor.execute(f"SELECT Discipline_laboratory,Discipline_lectures,Discipline_practices FROM rpd WHERE ID_OOP = {selected_oop_id}")
+    all_hours = sum([sum(i) for i in cursor])
 
     cursor.execute(f"SELECT Discipline_laboratory,Discipline_lectures,Discipline_practices FROM rpd, teachers, tea_dis WHERE rpd.Discipline_ID = tea_dis.Discipline_ID AND tea_dis.Teacher_ID = teachers.Teacher_ID AND teachers.Teacher_academic_degree != 'нет' AND rpd.ID_OOP = {selected_oop_id}")
-    partial_sum1 = sum([sum(i) for i in cursor])
-    n1 = round(100*partial_sum1/all_sum,2)
+    degree_hours = sum([sum(i) for i in cursor])
+    n1 = round(100*degree_hours/all_hours,2)
 
     cursor.execute(f"SELECT Discipline_laboratory,Discipline_lectures,Discipline_practices FROM rpd, teachers, tea_dis WHERE rpd.Discipline_ID = tea_dis.Discipline_ID AND tea_dis.Teacher_ID = teachers.Teacher_ID AND teachers.Teacher_practice_type = 'да' AND rpd.ID_OOP = {selected_oop_id}")
-    partial_sum2 = sum([sum(i) for i in cursor])
-    n2 = round(100*partial_sum2/all_sum,2)
+    pract_hours = sum([sum(i) for i in cursor])
+    n2 = round(100*pract_hours/all_hours,2)
 
-
-    output = f"Рассчет показателей для ООП {drop1_text.get()}\n\n" 
-    is_valid1 = 0
-    if n1 < ost_percent: is_valid1 = 1
-    output += f"Доля научно-педагогических работников, имеющих ученую степень: {n1}% - {is_valid1 * 'НЕ'} соответствует требованиям (более {ost_percent}%)\n\n"
-
-    is_valid2 = 0
-    if n2 < pract_percent: is_valid2 = 1
-
-    output += f"Доля работников из числа руководителей и(или) работников организаций, деятельность которых связана с направленностью реализуемой ОП: {n2}% = {is_valid2*'НЕ'} соответствует требованиям(более {pract_percent}%)"
     
-    lbl2.config(justify=tk.LEFT,text=output)
+    project_indicators = f"Рассчет показателей для ООП {oop_drop_text.get()}\n\n" 
+    is_valid1 = ''
+    if n1 < ost_percent: is_valid1 = 'НЕ'
+    project_indicators += f"Доля научно-педагогических работников, имеющих ученую степень: {n1}% - {is_valid1} соответствует требованиям (более {ost_percent}%)\n\n"
 
-    if(is_valid1+is_valid2!=0): btn2.pack()
+    is_valid2 = ''
+    if n2 < pract_percent: is_valid2 = 'НЕ'
+
+    project_indicators += f"Доля работников из числа руководителей и(или) работников организаций, деятельность которых связана с направленностью реализуемой ОП: {n2}% = {is_valid2} соответствует требованиям(более {pract_percent}%)"
+    
+    lbl2.config(justify=tk.LEFT,text=project_indicators)
+
+    if(is_valid1 + is_valid2!=''): get_recomendations_btn.pack()
     
     return
 
 
-def btn2_click():
-    lbl3.pack_forget()
-    output = "Для соответствия показателей:\n"
+def get_recomendations():
+    recomendations_label.pack_forget()
+    recomendations = "Для соответствия показателей:\n"
     if n1 < ost_percent:
-        n_ost = ceil(all_sum*ost_percent/100)
+        n_ost = ceil(all_hours*ost_percent/100)
         n_ost += n_ost%2
-        output += f"необходимо передать {n_ost-partial_sum1} часа(ов) преподователю(ям) со степенью\n"
+        recomendations += f"необходимо передать {n_ost-degree_hours} часа(ов) преподователю(ям) со степенью\n"
 
     if n2 < pract_percent:
-        n_pract = ceil(all_sum*pract_percent/100)
+        n_pract = ceil(all_hours*pract_percent/100)
         n_pract += n_pract%2
-        output += f"необходимо передать {n_pract-partial_sum2} часа(ов) преподователю(ям)-практику(ам)\n"
+        recomendations += f"необходимо передать {n_pract-pract_hours} часа(ов) преподователю(ям)-практику(ам)\n"
 
     
-    lbl3.config(justify=tk.LEFT,text=output)
-    lbl3.pack()
+    recomendations_label.config(justify=tk.LEFT,text=recomendations)
+    recomendations_label.pack()
     return
 
 
-drop1_text = tk.StringVar()
+
+oop_drop_text = tk.StringVar()
 
 lbl1 = tk.Label(root, text = "Выберите ООП")
 lbl1.pack()
 
-drop1 = ttk.Combobox(root, values = list(oop_ids.keys()), textvariable = drop1_text, state="readonly", width=70)
-drop1.bind("<<ComboboxSelected>>", drop1_selected)
-drop1.pack()
+oop_drop = ttk.Combobox(root, values = list(oop_ids.keys()), textvariable = oop_drop_text, state="readonly", width=70)
+oop_drop.bind("<<ComboboxSelected>>", oop_selected)
+oop_drop.pack()
 
-btn1 = tk.Button(root, text = "Start",state = "disabled", command=btn1_click)
-btn1.pack()
+get_project_indicators_btn = tk.Button(root, text = "показатели проектирования",state = "disabled", command=get_project_indicators)
+get_project_indicators_btn.pack()
 
 lbl2 = tk.Label(root, text="",width=80,wraplength=570)
 lbl2.pack()
 
-btn2 = tk.Button(root, text = "Предложить рекомендации", command=btn2_click)
+get_recomendations_btn = tk.Button(root, text = "Предложить рекомендации", command=get_recomendations)
 
 
-lbl3 = tk.Label(root, text="",width=80,wraplength=570)
+recomendations_label = tk.Label(root, text="",width=80,wraplength=570)
 
 
 root.mainloop()
